62f409a4da2581b2b1e96b62c42a218d46621a03
Darginec05 <devopsbanda@gmail.com>
2025-10-07 13:37:31 +0300

    refactor: remove unused Slate type definitions from accordion plugin

--- packages/plugins/accordion/src/slate.d.ts
+++
@@ -1,9 +0,0 @@
-import { SlateEditor, SlateElement, SlateElementTextNode } from '@yoopta/editor';
-
-declare module 'slate' {
-  type CustomTypes = {
-    Editor: SlateEditor;
-    Element: SlateElement;
-    Text: SlateElementTextNode;
-  }
-}
